b407863dc2b6a6489969c047f8a8097fb36c1569
Molecule81 <hkuloba33@gmail.com>
2025-01-26 16:37:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files (e.g., contact.html)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
 app.use(express.static('public'));
 
 // Mount contact route
