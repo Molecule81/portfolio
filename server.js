e63ddb6f7e33dece366a93059564e587db85b935
Molecule81 <hkuloba33@gmail.com>
2025-01-27 11:35:54 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 const app = express();
 const cors = require('cors');
 const path = require('path')
+const contactRoutes = require('./routes/contact');
+
 
 // Load environment variables
 dotenv.config();
@@ -12,6 +14,10 @@ dotenv.config();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Mount contact route
+app.use('/api/contact', contactRoutes);
+
 
 // Serve static files (e.g., contact.html)
 app.use(express.static(path.join(__dirname, 'public')));
@@ -32,10 +38,6 @@ app.get('/contact', (req, res) => {
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
-// Mount contact route
-const contactRoutes = require('./routes/contact');
-app.use('/api/contact', contactRoutes);
-
 // Start server
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
