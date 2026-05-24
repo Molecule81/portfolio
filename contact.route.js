8ff08149e01375439e284bfdc99c4c2f0dd37a4f
Molecule81 <hkuloba33@gmail.com>
2026-05-24 17:34:12 +0300

    Update contact.route.js

--- contact.route.js
+++ contact.route.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.MAIL_USER,
     pass: process.env.MAIL_PASS,
